39100085cb638c334636a8031c6a5427a4767e0f
Justin Brundage <jstnkbrundage@gmail.com>
2023-10-26 18:54:25 -0700

    maintain codes updated

--- maintain_lists.py
+++ maintain_lists.py
@@ -1,56 +1,37 @@
 import os
 
-### 
+def update_index_html(directory_path, depth=0):
+    if depth == 4:
+        return  # Limit recursion depth to 4 levels
 
-def print_directory_structure(path, indent=0):
-    if os.path.exists(path):
-        items = os.listdir(path)
-        for item in items:
-            item_path = os.path.join(path, item)
-            if os.path.isdir(item_path):
-                # Specify the directory path
-                directory_path = item_path
+    for item in os.listdir(directory_path):
+        item_path = os.path.join(directory_path, item)
+        if os.path.isdir(item_path):
+            index_html_path = os.path.join(item_path, 'index.html')
+            if os.path.exists(index_html_path):
+                with open(index_html_path, 'r') as index_html_file:
+                    index_html_content = index_html_file.read()
 
-                # Ensure the directory path exists
-                if os.path.exists(directory_path) and os.path.isdir(directory_path):
-                    # Check if 'index.html' exists in the 'states/' directory
-                    index_html_path = os.path.join(directory_path, 'index.html')
-                    if os.path.exists(index_html_path):
-                        # Read the content of 'index.html'
-                        with open(index_html_path, 'r') as index_html_file:
-                            index_html_content = index_html_file.read()
+                folder_list = [f for f in os.listdir(item_path) if os.path.isdir(os.path.join(item_path, f))]
 
-                        folder_list = os.listdir(directory_path)
+                list_items = []
+                for folder_name in folder_list:
+                    folder_link = os.path.join(folder_name, 'index.html').replace(os.path.sep, '/')
+                    if folder_link not in index_html_content:
+                        list_item = f'<li><a href="{folder_link}">{folder_name}</a></li>'
+                        list_items.append(list_item)
 
-                        # Filter only directories
-                        folder_list = [f for f in folder_list if os.path.isdir(os.path.join(directory_path, f))]
-
-                        # Generate the list items for directories not already present
-                        list_items = []
-                        for folder_name in folder_list:
-                            folder_link = os.path.join(folder_name, 'index.html').replace(os.path.sep, '/')
-                            # Check if the folder_link is already present in the HTML content
-                            if folder_link not in index_html_content:
-                                list_item = f'<li><a href="{folder_link}">{folder_name}</a></li>'
-                                list_items.append(list_item)
-
-                        # If there are new list items to add, update the 'index.html' content
-                        if list_items:
-                            placeholder = '<!-- Add links to future pages -->'
-                            updated_html_content = index_html_content.replace(placeholder, f'{placeholder}\n\t\t\t' + '\n\t\t\t'.join(list_items))
-
-                            # Write the updated HTML content back to 'index.html'
-                            with open(index_html_path, 'w') as index_html_file:
-                                index_html_file.write(updated_html_content)
-
-                            print("Updated 'index.html' with the new list items.")
-                        else:
-                            print("No new list items to add.")
-                    else:
-                        print("'index.html' does not exist in the 'states/' directory.")
+                if list_items:
+                    placeholder = '<!-- Add links to future pages -->'
+                    updated_html_content = index_html_content.replace(placeholder, f'{placeholder}\n\t\t\t' + '\n\t\t\t'.join(list_items))
+                    with open(index_html_path, 'w') as index_html_file:
+                        index_html_file.write(updated_html_content)
+                    print(f"Updated 'index.html' in {item_path} with new list items.")
                 else:
-                    print(f"The directory path '{directory_path}' does not exist or is not a directory.")
+                    print(f"No new list items to add in {item_path}.")
+
+            update_index_html(item_path, depth + 1)
 
 # Example usage:
 directory_path = "states/"  # Replace with your directory path
-print_directory_structure(directory_path)
+update_index_html(directory_path)
