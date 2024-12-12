2844caab9435f66ed3a42237495ad4667ba4a9c3
Justin Brundage <jstnkbrundage@gmail.com>
2024-12-12 15:55:47 -0800

    updated radio partnerships

--- sql_query/query2.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE schools(
-   "school_id"   VARCHAR(50)  NOT NULL
-  ,"school_name" VARCHAR(50) NOT NULL
-  ,district      VARCHAR(60) NOT NULL
-  ,county        VARCHAR(30) NOT NULL
-  ,state         VARCHAR(24) NOT NULL
-);
