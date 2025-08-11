4fa35885b4d931175e7f908bdc51878639e73809
Justin Brundage <jstnkbrundage@gmail.com>
2025-08-10 18:58:56 -0700

    logo is back to not brekaing

--- app.py
+++ app.py
@@ -166,7 +166,7 @@ def render_email_template(template_path: str, data: dict) -> str:
         template_content = f.read()
 
     # 2. Embed the logo image directly into the HTML
-    logo_path = '/static/images/logo.png'
+    logo_path = 'static/images/logo.png'
     with open(logo_path, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
         logo_data_uri = f"data:image/png;base64,{encoded_string}"
