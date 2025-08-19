ca4e74eae1b3eaa00e1abd70d6b0a205aa5f72b0
EngSmallz <119889053+EngSmallz@users.noreply.github.com>
2025-08-19 11:12:07 -0700

    Update app.py

--- app.py
+++ app.py
@@ -449,9 +449,9 @@ def first_of_month_job():
         print('Not the first.')
 
 def schedule_jobs():
-    schedule.every().tuesday.at("18:10").do(tuesday_job)
+    schedule.every().tuesday.at("18:30").do(tuesday_job)
     schedule.every().thursday.at("06:00").do(thursday_job)
-    schedule.every().day.at("18:10").do(daily_job)
+    schedule.every().day.at("18:30").do(daily_job)
     schedule.every().monday.at("06:00").do(monday_job)
     schedule.every().day.at("06:00").do(first_of_month_job)
 
